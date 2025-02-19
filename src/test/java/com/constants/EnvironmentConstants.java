eccfd17312d26c96a95adb17218b51a1fa16880a
PoojaEs <poojaeswarir@gmail.com>
2025-02-19 13:46:21 -0500

    push Excel reader class and changes
    (cherry picked from commit 4bbc65d8fd21d12900050369aaceb8774e4b9c6d)

---
+++ src/test/java/com/constants/EnvironmentConstants.java
@@ -0,0 +1,15 @@
+package com.constants;
+
+public class EnvironmentConstants {
+	
+	public static String USERNAME;
+	public static String PASSWORD;
+	
+	public static String APP_URL;
+	public static String BROWSER_TYPE;
+	public static String OS_TYPE;
+	
+	public static String EXCEL_FILE_PATH="./src/test/resources/TestData.xlsx";
+	public static String PROPERTIES_FILE_PATH="./src/test/resources/Config/Config.properties";
+
+}
